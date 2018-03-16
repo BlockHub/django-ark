848a08db9f41e9048b9551808ae00e0226126db7
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-16 18:01:48 +0100

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-ark',
-    version='0.1.1',
+    version='0.1.2',
     packages=['ark', 'ark.migrations'],
     url='https://github.com/BlockHub/django-ark',
     license='MIT',
