424c3b4f258ba756aa63b2da185d0c0ef946f75f
AbeIgnocious <k.l.kubat@gmail.com>
2018-03-16 18:46:18 +0100

    updated required_installs and incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-ark',
-    version='0.1.3',
+    version='0.1.4',
     packages=['ark', 'ark.migrations'],
     url='https://github.com/BlockHub/django-ark',
     license='MIT',
@@ -10,10 +10,10 @@ setup(
     author_email='karel@blockhub.nl',
     description='ark blockchain integration with Django',
     install_requires=[
-        'Arky','base58', 'certifi', 'cffi', 'chardet', 'Django', 'django-background-tasks',
+        'Arky','base58', 'certifi', 'cffi', 'chardet', 'Django', 'psycopg2-binary'
         'django-compat', 'django-filter', 'docopt', 'ecdsa', 'ECPy', 'future', 'graphene',
         'graphene-django', 'graphql-core', 'graphql-relay', 'hidapi', 'idna', 'iso8601',
-        'jsonfield', 'ledgerblue', 'Pillow', 'pkginfo', 'promise', 'protobuf', 'psycopg2',
+        'jsonfield', 'ledgerblue', 'Pillow', 'pkginfo', 'promise', 'protobuf',
         'pycparser', 'pycrypto', 'PyNaCl', 'python-dateutil', 'python-decouple', 'pytz',
         'requests', 'requests-toolbelt', 'Rx', 'singledispatch', 'six', 'tqdm',
         'typing', 'urllib3'
